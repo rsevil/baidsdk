19633ad851d11d9b4f6d967cc71d40756db1ea8e
Andres Degano <adegano@keetup.com>
2017-12-07 11:47:01 -0300

    [fix] [Title] : SUBJECT # More detailed explanatory text, if necessary. Wrap it to # about 72 characters. Please make sure to put some # effort in to making this descriptive enough for others # to read and understand.  [QA]: Has unit tests: No Reviewed by: None [JIRA]: https://keetup.atlassian.net/browse/test

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,5 @@ import { Observable } from 'rxjs/Rx';
 export declare class MibaLogin extends Common {
      get(): Observable<any>;
      login(): any;
-     clear(): any;
      init( apiUrl: string, appId: string, appName: string): any;
 }

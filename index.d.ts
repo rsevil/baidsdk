13d87fdfa8106f2b116f05cd46b4f0c6ee44ba79
Andres Degano <adegano@keetup.com>
2018-01-15 15:51:08 -0300

    [fix] index.d # # 	new file:   index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+import { Common } from './miba-login.common';
+import { Observable } from 'rxjs/Rx';
+export declare class MibaLogin extends Common {
+     get(): any;
+     login(): any;
+     clear(): any;
+     init( apiUrl: string, appId: string, appName: string): any;
+}
+
+export declare class BAIDApi extends NSObject {
+  static  alloc(): BAIDApi;
+  static  new(): BAIDApi;
+
+  }

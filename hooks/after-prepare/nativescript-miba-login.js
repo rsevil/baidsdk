36094ce60ee4b407c16f342af97954c8e628f9d5
Andres Degano <adegano@keetup.com>
2018-03-22 13:26:36 -0300

    hooks

---
+++ hooks/after-prepare/nativescript-miba-login.js
@@ -0,0 +1,1 @@
+module.exports = require("nativescript-miba-login/hooks/after-prepare/firebase-install-ios-entitlements.js");
---
+++ hooks/before-prepare/nativescript-miba-login.js
@@ -0,0 +1,1 @@
+module.exports = require("nativescript-miba-login/hooks/before-prepare/firebase-build-gradle.js");
